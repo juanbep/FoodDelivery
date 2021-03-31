d83b3dc91945c64a4a851a644a2b7f0b8531aac2
juanbep <56483953+juanbep@users.noreply.github.com>
2021-03-31 10:46:17 -0500

    versión 1.2

--- Documents/NetBeansProjects/ProyectoFoodDelivery/Restaurant-Client/src/main/java/co/unicauca/restaurant/client/domain/Admin.java
+++ Documents/NetBeansProjects/ProyectoFoodDelivery/Restaurant-Client/src/main/java/co/unicauca/restaurant/client/domain/Admin.java
@@ -5,16 +5,57 @@
  */
 package co.unicauca.restaurant.client.domain;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Beca98
  */
-public class Admin extends User{
-    
-    
-    
-    
-    
-    
-    
+public class Admin extends User {
+
+    /**
+     * El administrador puede tener 1 o varios restaurantes
+     */
+    private List<Restaurant> restaurants;
+
+    /**
+     * Cobstructor por defecto
+     */
+    public Admin() {
+        restaurants = new ArrayList<>();
+    }
+
+    /**
+     * super constructor
+     *
+     * @param restaurants
+     * @param atrUsername
+     * @param atrIdentificacion
+     * @param atrNames
+     * @param atrLastNames
+     * @param atrPassword
+     * @param atrCity
+     * @param atrAddress
+     * @param atrPhoneNumber
+     * @param atrType
+     */
+    public Admin(List<Restaurant> restaurants, String atrUsername, String atrIdentificacion, String atrNames, String atrLastNames, String atrPassword, String atrCity, String atrAddress, String atrPhoneNumber, String atrType) {
+        super(atrUsername, atrIdentificacion, atrNames, atrLastNames, atrPassword, atrCity, atrAddress, atrPhoneNumber, atrType);
+        this.restaurants = new ArrayList<>();
+    }
+
+    public void setRestaurants(List<Restaurant> restaurants) {
+        this.restaurants = restaurants;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        return restaurants;
+    }
+
+    public void addRestaurant(Restaurant restaurant) {
+        restaurants.add(restaurant);
+
+    }
+
 }
