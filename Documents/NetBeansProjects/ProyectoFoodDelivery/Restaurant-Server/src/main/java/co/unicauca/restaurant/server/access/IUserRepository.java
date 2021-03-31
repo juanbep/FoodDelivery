6b6ae36a3789d2e33f301a46837b593a79cd82cf
juanbep <56483953+juanbep@users.noreply.github.com>
2021-03-30 19:26:57 -0500

    versión 1.2

---
+++ Documents/NetBeansProjects/ProyectoFoodDelivery/Restaurant-Server/src/main/java/co/unicauca/restaurant/server/access/IUserRepository.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.unicauca.restaurant.server.access;
+
+/**
+ * Interfaz del repositorio de usuarios
+ *
+ * @author Beca98
+ */
+public interface IUserRepository {
+
+    /**
+     * Busca un Usuario por su ceduka
+     * @param id cedula del usuario 
+     * @return  objeto de tipo User 
+     */
+    
+    
+    
+}
