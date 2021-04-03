7c1454274df731e2d223c018d0a1670d3eb6d502
juanbep <56483953+juanbep@users.noreply.github.com>
2021-04-03 10:42:01 -0500

    versión 1.3

--- Documents/NetBeansProjects/ProyectoFoodDelivery/Restaurant-Server/src/main/java/co/unicauca/restaurant/server/domain/services/UserService.java
+++ Documents/NetBeansProjects/ProyectoFoodDelivery/Restaurant-Server/src/main/java/co/unicauca/restaurant/server/domain/services/UserService.java
@@ -5,10 +5,69 @@
  */
 package co.unicauca.restaurant.server.domain.services;
 
+import co.unicauca.restaurant.commons.domain.User;
+import co.unicauca.restaurant.commons.infra.JsonError;
+import co.unicauca.restaurant.server.access.IUserRepository;
+import com.google.gson.Gson;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Beca98
  */
 public class UserService {
-    
+
+    /**
+     * repositorio de Usuarios, Objeto de tipo IUserRepository.
+     */
+    IUserRepository repository;
+
+    /**
+     * Contructor por defecto.
+     */
+    public UserService() {
+    }
+
+    /**
+     * Constructor parametrizado. Hace inyeccion de dependencias
+     *
+     * @param repository repositorio de tipo IUserRepository
+     */
+    public UserService(IUserRepository repository) {
+        this.repository = repository;
+    }
+
+    /**
+     * Metodo encargado de buscar un Restaurante.
+     *
+     * @param parUserName
+     * @return objeto tipo Restaurante
+     */
+    public User findUser(String parUserName) {
+        return repository.findUser(parUserName);
+    }
+
+    /**
+     * Metodo encargado de crear un nuevo Usuario (User).
+     *
+     * @param parUserName objeto de tipo User.
+     * @return llamado a funcion createUser() de la interfaz IUserRepository.
+     */
+    public String CreateUser(User parUserName) {
+        List<JsonError> errors = new ArrayList<>();
+        if (parUserName.getAtrUserName().isEmpty() || parUserName.getAtrNames().isEmpty() || parUserName.getAtrLastNames().isEmpty()
+                || parUserName.getAtrIdentification().isEmpty() || parUserName.getAtrPassword().isEmpty()
+                || parUserName.getAtrCity().isEmpty() || parUserName.getAtrAddress().isEmpty() || parUserName.getAtrPhone().isEmpty()
+                || parUserName.getAtrType().isEmpty()) {
+            errors.add(new JsonError("400", "BAD_REQUEST", "TODOS LOS CAMPOS SON OBLIGATORIOS"));
+        }
+        if (!errors.isEmpty()) {
+            Gson gson = new Gson();
+            String errorJson = gson.toJson(errors);
+            return errorJson;
+        }
+        return repository.createUser(parUserName);
+    }
+
 }
